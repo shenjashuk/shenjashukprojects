58578ed62f1978cb0d725a469727db5f61200609
shenjashuk <58366584+shenjashuk@users.noreply.github.com>
2019-12-07 18:16:09 +0300

    Add files via upload

---
+++ Project2.cpp
@@ -0,0 +1,12 @@
+﻿// Project2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Evgenij";
+	return 0;
+}
+
+
